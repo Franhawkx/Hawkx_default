0585ed8aa1a2f4a641652c9a065706c29e1463b1
Adrian Caparros <adrian.caparros@hawkx.com>
2021-07-27 13:13:20 +0200

    Update hawkx.js

--- assets/hawkx.js
+++ assets/hawkx.js
@@ -1,3 +1,32 @@
 /*
 Hoja javascipt en sucio para trabajar el JS del tema
 ========================================================*/
+
+
+
+
+/*Petición de añadir el producto al carrito*/
+
+/*Petición para ver los productos del carrito*/
+
+
+function recuperarItemsCarrito(){
+
+
+    fetch('/cart.js')
+    .then(function(response) {
+    if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then(function(responseAsObject) {
+        console.log(responseAsObject);
+    })
+    .catch(function(error) {
+      console.log('Ha habido un problema: ', error);
+    });
+
+
+
+}
