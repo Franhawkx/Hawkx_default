5b5575272ebf8c193058af62df7a0d265433e478
Franhawkx <francisco.herrera@hawkx.com>
2021-07-27 18:09:49 +0200

    developing header

--- assets/hawkx.js
+++ assets/hawkx.js
@@ -1,3 +1,35 @@
 /*
 Hoja javascipt en sucio para trabajar el JS del tema
 ========================================================*/
+
+
+window.onload = function() {
+
+    // Función para añadir la clase sticky al header cuando se hace scroll Fran
+      const container_header = document.getElementById("shopify-section-header"); // Definimos el contenedor del elemento que va a ser sticky
+      const header = document.querySelectorAll("#shopify-section-header header")[0]; // Definimos el elemento que va a ser sticky
+      const Nsticky = document.querySelectorAll("#shopify-section-announcement-bar")[0].offsetHeight; // Definimos la cantidad de desplazamiento antes de aplicar sticky
+
+      // Añadimos el evento para escuvhar el desplazamiento
+      window.addEventListener("scroll", function() {sticky_menu(container_header, header, Nsticky);}, { passive: false });
+
+      // Comprobamos si existe desplazamiento vertical en scroll para aplicar sticky
+      if (pageYOffset >= Nsticky || scrollY >= Nsticky) {
+          container_header.style.height = header.offsetHeight + "px";
+          container_header.classList.add("sticky");
+      }
+
+}
+/* End onload
+========================================================*/
+
+//Función para añadir la clase sticky al header cuando se hace scroll Fran
+function sticky_menu($container, $element, $number) {
+  $container.style.height = $element.offsetHeight + "px";
+
+    if (pageYOffset >= $number || scrollY >= $number) {
+      $container.classList.add("sticky");
+    } else if(pageYOffset < $number || scrollY < $number){
+      $container.classList.remove("sticky");
+    }
+}
