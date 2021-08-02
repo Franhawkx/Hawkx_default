8431364f43a1b1ee033073806c164f0a27009538
Adrian Caparros <adrian.caparros@hawkx.com>
2021-08-02 11:46:21 +0200

    Update hawkx.js

--- assets/hawkx.js
+++ assets/hawkx.js
@@ -190,9 +190,18 @@ function crear_cliente(){
         };
         
         fetch(" /admin/api/2021-07/customers.json", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+        .then(function(response) {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+                return response.json();
+            })
+            .then(function(responseAsObject) {
+                console.log(responseAsObject);
+            })
+            .catch(function(error) {
+                console.log('Ha habido un problema: ', error);
+            });
 }
 
 
