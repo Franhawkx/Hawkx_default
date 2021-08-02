6b4e942aad4d05c7d3286017c561de2d351dda34
Guanajunior00 <javier.bellot@hawkx.com>
2021-08-02 12:41:58 +0200

    Update hawkx.js

--- assets/hawkx.js
+++ assets/hawkx.js
@@ -163,7 +163,7 @@ function crear_cliente(){
     
 
     
-    deleteAllCookies();
+    deleteCookies();
     fetch(url,request)
     .then(function(response) {
         
@@ -180,13 +180,12 @@ function crear_cliente(){
         });
         console.log(document.cookie);
 }
-function deleteAllCookies() {
-    var cookies = document.cookie.split(";");
-
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
-        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }
+function deleteCookies() {
+    var allCookies = document.cookie.split(';');
+    
+    // The "expire" attribute of every cookie is 
+    // Set to "Thu, 01 Jan 1970 00:00:00 GMT"
+    for (var i = 0; i < allCookies.length; i++)
+        document.cookie = allCookies[i] + "=;expires="
+        + new Date(0).toUTCString();
 }
