7710649d061764b580ee5d97fbdc64709804d27e
Adrian Caparros <adrian.caparros@hawkx.com>
2021-08-03 11:12:58 +0200

    Update hawkx.js

--- assets/hawkx.js
+++ assets/hawkx.js
@@ -252,7 +252,7 @@ function probandoEsto(){
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          "Authorization", "Basic YzY0YWY5ZGE3ZmNmZDQwYzQzMDE4OTE2YTg5M2UzN2Q6c2hwcGFfM2MzYzNhNzljNDUwMjA0Njg3NTUyY2U1ZTEwZDFkZDk="
+          "Authorization": "Basic YzY0YWY5ZGE3ZmNmZDQwYzQzMDE4OTE2YTg5M2UzN2Q6c2hwcGFfM2MzYzNhNzljNDUwMjA0Njg3NTUyY2U1ZTEwZDFkZDk="
         },
         body: JSON.stringify({
           query: `
