c81e2720ce9ac29b75f5d8714b80c2be3bb4595d
Adrian Caparros <adrian.caparros@hawkx.com>
2021-08-03 11:14:10 +0200

    Update hawkx.js

--- assets/hawkx.js
+++ assets/hawkx.js
@@ -268,8 +268,21 @@ function probandoEsto(){
             `
         }),
       })
-        .then((res) => res.json())
-        .then((result) => console.log(result));
+      .then(function (response) {
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        return response.json();
+    })
+    .then(function (responseAsObject) {
+        console.log("Hola");
+        for (let i = 0; i < responseAsObject.length; i++) {
+            console.log(responseAsObject);
+        }
+    })
+    .catch(function (error) {
+        console.log('Ha habido un problema: ', error);
+    });
       
 
 }
